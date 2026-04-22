34ce8e90fe5a75dae0928116bf6ff646e544ffad
koji <kk2796@nyu.edu>
2026-04-22 00:17:56 -0400

    modify package.json for npm page

--- dist/cli.js
+++ dist/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { readFileSync } from "node:fs";
+import { readFileSync } from 'node:fs';
 import { applyChanges, createGitHubResolver, formatReport, inspectWorkflows, parseArgs, } from "./lib.js";
 const packageJson = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), 'utf8'));
 const VERSION = packageJson.version ?? '0.0.0';
